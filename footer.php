0067e17aaf07aec7a4a19755b905725c41b19883
frostkom <martin.fors@frostkom.se>
2016-05-24 11:37:46 +0200

    Include global variables in function to show title

--- footer.php
+++ footer.php
@@ -19,8 +19,10 @@
 
 		list($options_params, $options) = get_params();
 
-		mf_enqueue_script('script_nav', get_bloginfo('template_url')."/include/jquery.nav.js");
-		mf_enqueue_script('script_parallax', get_bloginfo('template_url')."/include/script.js", array('override_bg' => isset($options['header_override_bg_with_page_bg']) && $options['header_override_bg_with_page_bg'] == 2));
+		$template_url = get_bloginfo('template_url');
+
+		mf_enqueue_script('script_nav', $template_url."/include/jquery.nav.js");
+		mf_enqueue_script('script_parallax', $template_url."/include/script.js", array('override_bg' => isset($options['header_override_bg_with_page_bg']) && $options['header_override_bg_with_page_bg'] == 2));
 
 		wp_footer();
 
